5047d70270fe3235e5d4bbde03d54ef8a84c1d44
ram <bvn.ramkumar@gmail.com>
2026-01-03 07:57:46 +0530

    data fetched in hero component

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,21 +1,32 @@
-import React from "react";
-import hero from"/images/hero/hero.png";
+import React, { useEffect, useState } from "react";
+import hero from "/images/hero/hero.png";
+const IMAGE_BASE =
+  "https://cuteweb.in/sandbox/budget/admin/uploads/slider/";
 
 const Hero = () => {
+  const [hero, setHero] = useState(null);
+  useEffect(()=>{
+    fetch("https://cuteweb.in/sandbox/budget/admin/api/getHomePageSlider")
+      .then((res) => res.json())
+      .then((data) => setHero(data))
+      
+      .catch((err) => console.error(err));
+  },[]);
+   if (!hero) return null;
   return (
+    
     <section className="cream-bg h-screen flex items-center py-5 px-10 mb-5 ">
+     
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2  gap-10">
         <div>
           <p className="text-secondary font-semibold mb-3">
             Only The Best Furniture
           </p>
           <h1 className="third-color text-5xl font-bold leading-tight">
-            Discover Your Dream Furniture Today
+           {hero?.title }
           </h1>
           <p className="text-theme mt-6 max-w-md">
-            Explore our exquisite collection of furniture that combines style,
-            comfort, and quality. Transform your living space with pieces that
-            reflect your unique taste and elevate your home decor.
+             {hero?.content}
           </p>
           <button
             className="
@@ -25,11 +36,16 @@ const Hero = () => {
         transition
       "
           >
-            Shop Collection →
+            Veiw →
           </button>
         </div>
-        <div>
-            <img src={hero} className="w-full h-[420px] object-cover rounded-2xl" loading="lazy"/>
+        <div className="  h-[220px] sm:h-[300px] md:h-[380px] lg:h-[420px]">
+          <img
+            src={IMAGE_BASE + hero.image}
+            alt={hero?.title}
+            className="w-full  object-cover rounded-2xl"
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
