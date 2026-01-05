12a729e8f2ed45b63f9a0bd6affdb93159936617
ram <bvn.ramkumar@gmail.com>
2026-01-05 11:20:58 +0530

    axios.js : base url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api` || "https://cuteweb.in/sandbox/budget/admin/api",
+  baseURL:  "https://cuteweb.in/sandbox/budget/admin/api",
   headers: {
     "Content-Type": "application/json",
   },
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api/axios";
 
+
 const Hero = () => {
   const [hero, setHero] = useState(null);
 
